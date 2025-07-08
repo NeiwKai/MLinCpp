3a25e35dc9977f8c5093ccfe6d0062760fb72ff0
Suppanat Timngarm <111691144+NeiwKai@users.noreply.github.com>
2025-07-08 16:29:49 +0700

    finish support many features

---
+++ LogisticRegression/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <typeinfo>
+#include <random>
+#include <vector>
+#include <algorithm>
+#include <xtensor.hpp>
+#include <xtensor-blas/xlinalg.hpp>
+#include <cmath>
+
+#include "../../metrics/accuracy_score.hpp"
+#include "../../selection/train_val_split.hpp"
+#include "LogisticRegression.hpp"
+
+int main() {
+  xt::random::seed(42);
+  xt::xarray<double> X = xt::random::randn<double>({100, 5}, 0.0, 1.0);
+  xt::xarray<double> Y = xt::random::randint<int>({100}, 0, 2);
+  
+  //xt::xarray<double> X_train, X_val, Y_train, Y_val;
+  auto [X_train, X_val, Y_train, Y_val] = train_val_split(X, Y, 0.2, 42);
+  //std::cout << typeid(X_train).name();
+  LogisticRegression lr(0.01, 1000);
+  lr.fit(X_train, Y_train);
+  xt::xarray<double> Y_pred = lr.predict(X_val);
+  std::cout << Y_pred << std::endl;
+  std::cout << "Accuracy: " << accuracy_score(Y_val, Y_pred);
+
+  return 0;
+}
